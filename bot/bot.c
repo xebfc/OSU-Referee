72c0a49a4195b15661283ecb74ec8ef13755d126
muto <316663878@qq.com>
2019-07-18 07:54:09 +0800

    add BB cmd

--- bot/bot.c
+++ bot/bot.c
@@ -17,7 +17,8 @@ static void utf8_command(const char* cmd);
 static hexchat_plugin* ph;      /* plugin handle */
 static int enable = 1;
 
-static int y_cb(char* word[], char* word_eol[], void* userdata)
+static int
+y_cb(char* word[], char* word_eol[], void* userdata)
 {
 	wchar_t* w = from_utf8(word[4]);
 	if (wcscmp(w, L":!y") != 0)
@@ -36,9 +37,23 @@ end:
 	return HEXCHAT_EAT_NONE;
 }
 
+static int
+bb_cb(char* word[], char* word_eol[], void* userdata)
+{
+	utf8_commandf("MSG BanchoBot %s", word_eol[2]);
+	return HEXCHAT_EAT_ALL;
+}
+
+static int
+bancho_cb(char* word[], char* word_eol[], void* userdata)
+{
+	return HEXCHAT_EAT_NONE;
+}
+
 //-------------------------------------------------------------------------------
 
-static void utf8_commandf(const char* format, ...)
+static void
+utf8_commandf(const char* format, ...)
 {
 	va_list args;
 	va_start(args, format);
@@ -52,16 +67,32 @@ static void utf8_commandf(const char* format, ...)
 	va_end(args);
 }
 
-static void utf8_command(const char* cmd)
+static void
+utf8_command(const char* cmd)
 {
 	char* cmd_utf8 = _G_LOCALE_TO_UTF8(cmd);
 	hexchat_command(ph, cmd_utf8);
 	free(cmd_utf8);
 }
 
+static hexchat_hook*
+utf8_hook_command(const char* name,
+	int pri,
+	int (*callback) (char* word[], char* word_eol[], void* user_data),
+	const char* help_text,
+	void* userdata)
+{
+	char* help_text_utf8 = _G_LOCALE_TO_UTF8(help_text);
+	hexchat_hook* hook = hexchat_hook_command(ph, name, pri, callback, help_text_utf8, userdata);
+	free(help_text_utf8);
+	return hook;
+}
+
 int
 hexchat_plugin_init(hexchat_plugin* plugin_handle,
-	char** plugin_name, char** plugin_desc, char** plugin_version,
+	char** plugin_name,
+	char** plugin_desc,
+	char** plugin_version,
 	char* arg)
 {
 	/* we need to save this for use with any hexchat_* functions */
@@ -73,6 +104,9 @@ hexchat_plugin_init(hexchat_plugin* plugin_handle,
 	*plugin_version = PVERSION;
 
 	hexchat_hook_server(ph, "PRIVMSG", HEXCHAT_PRI_NORM, y_cb, NULL);
+
+	utf8_hook_command("BB", HEXCHAT_PRI_NORM, bb_cb, "Usage: BB <message> 给BanchoBot发送消息", NULL);
+	hexchat_hook_server(ph, "PRIVMSG", HEXCHAT_PRI_NORM, bancho_cb, NULL);
 
 	hexchat_printf(ph, "%s loaded successfully!\n", PNAME);
 	return 1;       /* return 1 for success */
