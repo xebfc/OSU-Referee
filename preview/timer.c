8d4a658ed3602f0ff26d67d2585d5de880cac3c0
muto <316663878@qq.com>
2019-09-17 09:08:57 +0800

    再次提高精度，并非都是自旋+阻塞

--- preview/timer.c
+++ preview/timer.c
@@ -4,6 +4,9 @@
 #include "timer.h"
 #include "stopwatch.h"
 
+// 最低支持1ms分辨率，否则将超出范围
+#define getResolution(timer) (timer->uResolution ? timer->uResolution : 1)
+
 typedef struct
 {
     UINT            uDelay;
@@ -13,9 +16,8 @@ typedef struct
     UINT            fuEvent;
 
     DWORD           lpThreadId; // 线程标识符
-    HANDLE          hThread;    // 线程的句柄
+    HANDLE          hThread;    // 线程句柄
     BOOL            __close;
-    HANDLE          __cLock;
     BOOL            __error;
 } timer_t;
 
@@ -31,22 +33,33 @@ DWORD WINAPI lpStartAddress(LPVOID lpParameter)
     Stopwatch_StartNew(&s);
 
     // 固定时间框架
-    UINT next = 0;
+    UINT next = 0, delay;
     while (TRUE)
     {
         next += timer->uDelay;
         while ((UINT)Stopwatch_ElapsedMilliseconds(&s) < next)
-            Sleep(next - (UINT)s.ElapsedMilliseconds); // 自旋 + 阻塞
+        {
+            if (!timer->uResolution)
+                continue; // 自旋的精准度最高，尽可能准确地发生周期性事件
 
-        WaitForSingleObject(timer->__cLock, 0);
-        if (timer->lpTimeProc != NULL)
-            timer->lpTimeProc(timer->lpThreadId, 0, timer->dwUser, 0, 0); // 触发任务
-        ReleaseMutex(timer->__cLock);
+            // Sleep(1) 会在 1ms 与 2ms 两种状态间跳动，也就是可能会产生 +1ms 多的误差。如果需要修正这个误差，
+            // 可以在阻塞 n 毫秒时，使用 Sleep(n-1)，并通过 Stopwatch 计时，剩余等待时间用 Sleep(0) 或自旋来补充。
+            // Sleep(0) 在 CPU 高负载情况下非常不稳定，实测可能会阻塞高达 6ms 时间，所以可能会产生更多的误差。
+            // 因此误差修正最好通过自旋方式实现。
+            if ((delay = next - (UINT)s.ElapsedMilliseconds) > 1)
+                Sleep(delay - 1);
+        }
+
+        timer->lpTimeProc(timer, 0, timer->dwUser, 0, 0); // 触发任务
 
         if (timer->__close)
             break; // 终止线程运行的最佳方法是让它的线程函数返回
     }
 
+    timeEndPeriod(getResolution(timer));
+    CloseHandle(timer->hThread); // 关闭线程句柄不会终止关联的线程或删除线程对象
+
+    free(timer);
     return 0;
 }
 
@@ -66,7 +79,7 @@ MMRESULT TIMER_timeSetEvent(
         return NULL;
 
     timer->uDelay = uDelay;
-    timer->uResolution = uResolution == 0 ? 1 : timer->uResolution; // 最低支持1ms分辨率，否则将超出范围
+    timer->uResolution = uResolution;
     timer->lpTimeProc = lpTimeProc;
     timer->dwUser = dwUser;
     timer->fuEvent = fuEvent;
@@ -83,19 +96,17 @@ MMRESULT TIMER_timeSetEvent(
     // 正是由于这一原因，High 通常是实时程序的最好选择。
     if (!SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS)
         || !SetThreadPriority(timer->hThread, THREAD_PRIORITY_HIGHEST)
-        || timeBeginPeriod(timer->uResolution) == TIMERR_NOCANDO)
+        || timeBeginPeriod(getResolution(timer)) == TIMERR_NOCANDO)
     {
         timer->__error = TRUE;
         ResumeThread(timer->hThread);
 
-        MWFMO(timer->hThread, INFINITE);
+        MWFMO(timer->hThread, INFINITE); // Wait Function
         CloseHandle(timer->hThread);
-        goto err; // 对于高精度来说任何一项失败都是致命的。
+        goto err; // 对于高精度来说任何一项失败都是致命的
     }
 
-    timer->__cLock = CreateMutex(NULL, FALSE, NULL);
     timer->__error = FALSE;
-
     ResumeThread(timer->hThread);
     return timer;
 
@@ -117,16 +128,8 @@ MMRESULT TIMER_timeKillEvent(UINT uTimerID)
     if (!lpThreadId || lpThreadId != timer->lpThreadId)
         return MMSYSERR_INVALPARAM;
 
-    MWFMO(timer->__cLock, INFINITE);
     timer->__close = TRUE; // TerminateThread
     if ((timer->fuEvent & TIME_KILL_SYNCHRONOUS) == TIME_KILL_SYNCHRONOUS)
-        timer->lpTimeProc = NULL;
-    ReleaseMutex(timer->__cLock);
-
-    MWFMO(timer->hThread, INFINITE); // Wait Functions
-    timeEndPeriod(timer->uResolution);
-    CH(timer->__cLock);
-    CloseHandle(timer->hThread); // 关闭线程句柄不会终止关联的线程或删除线程对象
-    free(timer);
+        MWFMO(timer->hThread, INFINITE);
     return TIMERR_NOERROR;
 }
